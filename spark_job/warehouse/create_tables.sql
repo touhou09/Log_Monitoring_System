f9d066db29a904b78567edbfe3500d83c33bd7eb
touhou09 <touhou3178@gmail.com>
2025-04-29 09:35:07 +0900

    spark tunning and tables setting done

--- spark_job/warehouse/create_tables.sql
+++ spark_job/warehouse/create_tables.sql
@@ -1,63 +1,63 @@
 CREATE DATABASE IF NOT EXISTS analytics;
 
-CREATE TABLE analytics.fact_auth (
+CREATE TABLE IF NOT EXISTS analytics.fact_auth (
     timestamp DateTime,
     user_id String,
     level String
 ) ENGINE = MergeTree() ORDER BY timestamp;
 
-CREATE TABLE analytics.fact_order (
+CREATE TABLE IF NOT EXISTS analytics.fact_order (
     timestamp DateTime,
     user_id String,
     product_id String,
     level String
 ) ENGINE = MergeTree() ORDER BY timestamp;
 
-CREATE TABLE analytics.fact_payment (
+CREATE TABLE IF NOT EXISTS analytics.fact_payment (
     timestamp DateTime,
     user_id String,
     amount UInt32,
     level String
 ) ENGINE = MergeTree() ORDER BY timestamp;
 
-CREATE TABLE analytics.fact_notify (
+CREATE TABLE IF NOT EXISTS analytics.fact_notify (
     timestamp DateTime,
     user_id String,
     notif_type String,
     level String
 ) ENGINE = MergeTree() ORDER BY timestamp;
 
-CREATE TABLE analytics.fact_inventory (
+CREATE TABLE IF NOT EXISTS analytics.fact_inventory (
     timestamp DateTime,
     item_id String,
     remaining UInt8,
     level String
 ) ENGINE = MergeTree() ORDER BY timestamp;
 
-CREATE TABLE analytics.fact_etc (
+CREATE TABLE IF NOT EXISTS analytics.fact_etc (
     timestamp DateTime,
     domain String,
     level String
 ) ENGINE = MergeTree() ORDER BY timestamp;
 
-CREATE TABLE analytics.dim_user (
+CREATE TABLE IF NOT EXISTS analytics.dim_user (
     user_id String
 ) ENGINE = MergeTree() ORDER BY user_id;
 
-CREATE TABLE analytics.dim_item (
+CREATE TABLE IF NOT EXISTS analytics.dim_item (
     item_id String,
     product_id String
 ) ENGINE = MergeTree() ORDER BY item_id;
 
-CREATE TABLE analytics.dim_status (
+CREATE TABLE IF NOT EXISTS analytics.dim_status (
     level String
 ) ENGINE = MergeTree() ORDER BY level;
 
-CREATE TABLE analytics.dim_logger (
+CREATE TABLE IF NOT EXISTS analytics.dim_logger (
     logger String
 ) ENGINE = MergeTree() ORDER BY logger;
 
-CREATE TABLE analytics.dim_time (
+CREATE TABLE IF NOT EXISTS analytics.dim_time (
     timestamp DateTime,
     year UInt16,
     month UInt8,
@@ -66,6 +66,6 @@ CREATE TABLE analytics.dim_time (
     minute UInt8
 ) ENGINE = MergeTree() ORDER BY timestamp;
 
-CREATE TABLE analytics.dim_domain (
+CREATE TABLE IF NOT EXISTS analytics.dim_domain (
     domain String
 ) ENGINE = MergeTree() ORDER BY domain;
